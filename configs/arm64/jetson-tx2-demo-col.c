d56b3a0ea14fc4d1e057a0cb8f8aebd299c3365c
Luca Miccio <206497@studenti.unimore.it>
2019-03-27 13:18:48 +0100

    Configs: jetson-tx2: add cache-coloring examples
    Add colored root cell configuration for the NVIDIA TX2 development board
    along with an example of a non root-cell configuration for a bare metal inmate.
    In these configurations we are using the automated colored allocation and we
    are assigning to each memory regions half of the available colors on the
    platform.
    
    Signed-off-by: Luca Miccio <lucmiccio@gmail.com>
    Signed-off-by: Marco Solieri <ms@xt3.it>

---
+++ configs/arm64/jetson-tx2-demo-col.c
@@ -0,0 +1,76 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Coloring Configuration for gic-demo or uart-demo inmate on Nvidia Jetson TX2:
+ * 1 CPU, 1 MB RAM, serial port 0
+ * Coloring configuration:
+ * 0xffff0000 ->	half of available colors, i.e.:
+ * 			L2 size: 1 MiB
+ *  			Memory  mapped: 1 MiB
+ * 			Memory reserved by color selection: 50 MiB
+ *
+ * Copyright (c) UniversitÃ  di Modena e Reggio Emilia, 2019
+ *
+ * Authors:
+ *  Luca Miccio <lucmiccio@gmail.com>
+ *  Marco Solieri <ms@xt3.it>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[3];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
+		.name = "jetson-tx2-demo-col",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+
+		.console = {
+			.address = 0x3100000,
+			.type = JAILHOUSE_CON_TYPE_8250,
+			.flags = JAILHOUSE_CON_ACCESS_MMIO |
+				 JAILHOUSE_CON_REGDIST_4,
+		},
+	},
+
+	.cpus = {
+		0x8,
+	},
+
+	.mem_regions = {
+		/* UART */ {
+			.phys_start = 0x3100000,
+			.virt_start = 0x3100000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* RAM */ {
+			.virt_start = 0,
+			.size = 0x100000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE |
+				JAILHOUSE_MEM_COLORED_CELL,
+			.colors = 0xffff0000,
+		},
+		/* communication region */ {
+			.virt_start = 0x80000000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_COMM_REGION,
+		},
+	},
+};
