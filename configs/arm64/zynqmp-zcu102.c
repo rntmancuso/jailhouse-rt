12c6d77e2b952ec1b2d2a96a7e0d51e32e56c2e7
Rohan Tabish <rtabish@illinois.edu>
2020-09-07 14:28:50 -0500

    Fixed data structure

--- configs/arm64/zynqmp-zcu102.c
+++ configs/arm64/zynqmp-zcu102.c
@@ -20,7 +20,7 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[24];
+	struct jailhouse_memory mem_regions[17];
 	struct jailhouse_irqchip irqchips[1];
 	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
