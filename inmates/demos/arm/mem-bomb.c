89518c64b0e5a25507b0541bdd38665e25a5dd41
Renato Mancuso <rntmancuso@gmail.com>
2020-10-25 08:16:53 -0400

    Added translation debugging and timing of read operations for memory bombs

--- inmates/demos/arm/mem-bomb.c
+++ inmates/demos/arm/mem-bomb.c
@@ -14,11 +14,11 @@
 
 #include <inmate.h>
 #include <gic.h>
-#include <jailhouse/memguard-common.h>
 #include <asm/sysregs.h>
+#include <jailhouse/memguard-common.h>
 
 #define MEM_SIZE_MB         4 /* Memory size will be 4 MB */
-#define MEM_SIZE            (MEM_SIZE_MB * 1024 * 1024)
+#define MEM_SIZE            (MEM_SIZE_MB * 1024 * 1024) 
 #define LINE_SIZE           (64) //Cache line size is 64 bytes
 #define BIT(x)              (1UL << x)
 
@@ -67,26 +67,37 @@ void do_writes(volatile struct control * ctrl);
 /* Perform write-only iterations over the memory buffer */
 void do_reads_writes(volatile struct control * ctrl);
 
+/* Print some info about the memory setup in the inmate */
+void print_mem_info(void);
 
 /* Perform read-only iterations over the memory buffer */
 void do_reads(volatile struct control * ctrl)
 {
 	int i = 0;
 	int size = ctrl->size;
-
+	int loops = 0;
+	unsigned long cycles;
+	unsigned long total = 0;
+	
 	crc = 0;
 
 	if (ctrl->command & CMD_VERBOSE)
 		print("Started READ accesses with size %d.\n", size);
 
 	while (ctrl->command & CMD_ENABLE) {
+		magic_timing_begin(&cycles);
 		for (i = 0; i < size; i += LINE_SIZE) {
 			crc += buffer[i];
 		}
+		magic_timing_end(&cycles);
+		++loops;
+		total += cycles;
 	}
 
-	if (ctrl->command & CMD_VERBOSE)
+	if (ctrl->command & CMD_VERBOSE) {
 		print("Done with READ accesses. Check = 0x%08llx\n", crc);
+		print("\tAvg. Time: %ld\n", total / loops);
+	}
 
 }
 
@@ -95,12 +106,12 @@ void do_writes(volatile struct control * ctrl)
 {
 	int i = 0;
 	int size = ctrl->size;
-
+	
 	crc = 0;
 
 	if (ctrl->command & CMD_VERBOSE)
 		print("Started WRITE accesses with size %d.\n", size);
-
+	
 	while (ctrl->command & CMD_ENABLE) {
 		for (i = 0; i < size; i += LINE_SIZE) {
 			buffer[i] += i;
@@ -117,7 +128,7 @@ void do_reads_writes(volatile struct control * ctrl)
 {
 	int i = 0;
 	int size = ctrl->size;
-
+	
 	crc = 0;
 
 	if (ctrl->command & CMD_VERBOSE)
@@ -125,7 +136,7 @@ void do_reads_writes(volatile struct control * ctrl)
 
 	/* The top half will be written, the bottom half will be read */
 	size = size / 2;
-
+	
 	while (ctrl->command & CMD_ENABLE) {
 		for (i = 0; i < size; i += LINE_SIZE) {
 			buffer[i] += buffer[i+size];
@@ -137,31 +148,47 @@ void do_reads_writes(volatile struct control * ctrl)
 
 }
 
-static void test_translation(unsigned long addr)
+/* Print some info about the memory setup in the inmate */
+void print_mem_info(void)
 {
-        unsigned long par;
-        asm volatile("at s1e1r, %0" : : "r"(addr));
-        arm_read_sysreg(PAR_EL1, par);
-        print("Translated 0x%08lx -> 0x%08lx\n", addr, par);
+	/* Read and print value of the SCTRL register */
+	unsigned long sctlr, tcr;
+	arm_read_sysreg(SCTLR, sctlr);
+	arm_read_sysreg(TRANSL_CONT_REG, tcr);
+
+	print("SCTLR_EL1 = 0x%08lx\n", sctlr);
+	print("TCR_EL1 = 0x%08lx\n", tcr);
 }
 
+static void test_translation(unsigned long addr)
+{
+	unsigned long par;
+	asm volatile("at s1e1r, %0" : : "r"(addr));
+
+	arm_read_sysreg(PAR_EL1, par);
+
+	print("Translated 0x%08lx -> 0x%08lx\n", addr, par);
+}
 
 
 void inmate_main(void)
 {
-	printk("Test\n");
 	volatile struct control * ctrl = (volatile struct control *)(CMD_REGION_BASE);
 
 	/* Set the ID of this bomb from the passed command */
 	id = ctrl->command >> CMD_BOMB_ID_SHIFT;
 	mg_params.budget_time = 1000;
 	mg_params.flags = 1;
-
+	
 	print("Memory Bomb Started.\n");
 
+	print_mem_info();
 	test_translation((unsigned long)buffer);
 	jailhouse_call_arg1(JAILHOUSE_HC_QOS+1, (unsigned long)buffer);
 
+	jailhouse_call_arg1(JAILHOUSE_HC_QOS+1, (unsigned long)0x7500000UL);
+	jailhouse_call_arg1(JAILHOUSE_HC_QOS+1, (unsigned long)0x6500000UL);
+	
 	/* Main loop */
 	while(1) {
 		/* Idle while the enable bit is cleared */
@@ -172,7 +199,7 @@ void inmate_main(void)
 			mg_params.budget_memory = ctrl->mg_budget;
 			jailhouse_call_arg1(JAILHOUSE_HC_MEMGUARD, (unsigned long)&mg_params);
 		}
-
+		
 		if ((ctrl->command & CMD_DO_READS) &&
 		    (ctrl->command & CMD_DO_WRITES))
 			do_reads_writes(ctrl);
@@ -189,6 +216,6 @@ void inmate_main(void)
 			ctrl->command &= ~CMD_ENABLE;
 		}
 	}
-
+	
 	halt();
 }
