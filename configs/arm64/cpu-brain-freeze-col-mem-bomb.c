dcaa12b8a50aeed76e8a850131060085682c8af7
Denis <denis.hoornaert@tum.de>
2021-01-20 14:31:23 +0100

    New configuration for the cpu-brainfreeze experiments

---
+++ configs/arm64/cpu-brain-freeze-col-mem-bomb.c
@@ -0,0 +1,112 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Configuration for demo inmate on Xilinx ZynqMP ZCU102 eval board:
+ * 1 CPU, 64K RAM, 1 serial port
+ *
+ * Copyright (c) Siemens AG, 2016
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define xstr(s) str(s)
+#define str(s) #s
+
+#define BOMB_ID               0
+#define BOMB_CPU              1 << (BOMB_ID + 1)
+#define MAIN_SIZE             0x500000
+#define MAIN_PHYS_START       (0x0801200000 + BOMB_ID * 16 * MAIN_SIZE)
+#define MAIN_PHYS_START_SCHIM (0x1001200000 + (BOMB_ID+1) * 16 * MAIN_SIZE) // 0x10_0000_0000 as dual ported SchIm is used
+#define COMM_PHYS_ADDR        (0x060700000 + BOMB_ID * 0x1000)
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[3];
+	struct jailhouse_memory_colored col_mem[2];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
+		.name = "cbf-col-mem-bomb-" xstr(BOMB_ID),
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_memory_regions_colored = ARRAY_SIZE(config.col_mem),
+		.num_irqchips = 0,
+		.num_pci_devices = 0,
+
+		.console = {
+			.address = 0xff010000,
+			.type = JAILHOUSE_CON_TYPE_XUARTPS,
+			.flags = JAILHOUSE_CON_ACCESS_MMIO |
+				 JAILHOUSE_CON_REGDIST_4,
+		},
+	},
+
+	.cpus = {
+		BOMB_CPU,
+	},
+
+	.mem_regions = {
+		/* UART */ {
+			.phys_start = 0xff010000,
+			.virt_start = 0xff010000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+
+		/* Control interface */ {
+			.phys_start = COMM_PHYS_ADDR,
+			.virt_start = 0x500000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+			         JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* communication region */ {
+			.virt_start = 0x80000000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_COMM_REGION,
+		},
+	},
+
+	.col_mem = {
+		{
+			/* RAM */
+			.memory = {
+				.phys_start = MAIN_PHYS_START,
+				.virt_start = 0,
+				.size = MAIN_SIZE,
+				.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+					JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+			},
+
+			/* Assigning 1/4 of the colors */
+			.colors=0x00f0,
+		},
+		{
+			/* RAM through SchIM */
+			.memory = {
+				.phys_start = MAIN_PHYS_START_SCHIM,
+				.virt_start = 0xa00000,
+				.size = MAIN_SIZE,
+				.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+					JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+			},
+
+			/* Assigning 1/4 of the colors */
+			.colors=0x000f,
+		},
+	},
+
+};
