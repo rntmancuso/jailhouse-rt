3089de37c9ad3dba8e0ee1dbeb62bb7a8cc9a9d1
Denis <denis.hoornaert@tum.de>
2021-02-18 20:53:51 +0100

    Fix phys/virt issues

--- hypervisor/arch/arm64/coloring.c
+++ hypervisor/arch/arm64/coloring.c
@@ -25,6 +25,8 @@
 #include <jailhouse/mmio.h>
 #include <asm/coloring.h>
 
+#define SCHIM_RECOLORING_ENABLE 1
+
 #define col_print(fmt, ...)			\
 	printk("[COL] " fmt, ##__VA_ARGS__)
 
@@ -90,13 +92,13 @@ static int coloring_cache_detect(void)
 	 * present in the system. */
 	u64 reg, type;
 	int i;
-	
+
 	arm_read_sysreg(clidr_el1, reg);
 
 	/* Initialize this field to detect when no suitable caches
 	 * have been found */
 	cache.level = -1;
-	
+
 	for (i = 1; i <= MAX_CACHE_LEVELS; ++i) {
 		u64 geom, assoc, ls, sets;
 		type = FIELD_GET(CLIDR_CTYPE(i), reg);
@@ -126,18 +128,18 @@ static int coloring_cache_detect(void)
 			ls = 1 << (4 + FIELD_GET(CCSIDR_LINE_SIZE, geom));
 			assoc = FIELD_GET(CCSIDR_ASSOC, geom) + 1;
 			sets = FIELD_GET(CCSIDR_NUM_SETS, geom) + 1;
-			
+
 			col_print("\t\tTotal size (I): %lld\n", ls * assoc * sets);
 			col_print("\t\tLine size (I): %lld\n", ls);
 			col_print("\t\tAssoc. (I): %lld\n", assoc);
 			col_print("\t\tNum. sets (I): %lld\n", sets);
 
 		}
-		
+
 		/* Perform coloring at the last unified cache level */
 		if (type == CLIDR_CTYPE_UNIFIED) {
 			cache.level = i;
-			
+
 			cache.size = ls * assoc * sets;
 			cache.line_size = ls;
 			cache.way_size = ls * sets;
@@ -145,13 +147,13 @@ static int coloring_cache_detect(void)
 			cache.colors = sets / (PAGE_SIZE / ls);
 
 			/* Compute the max. number of colors */
-			col_print("\t\tNum. colors: %lld\n", cache.colors);			
+			col_print("\t\tNum. colors: %lld\n", cache.colors);
 		}
-		
+
 	}
 
 	col_print("\tNOTE: L%d Cache selected for coloring.\n", cache.level);
-	
+
 	/* Backward compatibility properties. TODO: remove these */
 	cache.fragment_unit_size = PAGE_SIZE;
 	cache.fragment_unit_offset = cache.way_size;
@@ -159,7 +161,7 @@ static int coloring_cache_detect(void)
 	return (cache.level == -1);
 }
 
-/** 
+/**
  *   Perform a copy of memory from a non-colored space to a colored
  *   space. The two spaces could be overlapping in physical memory, so
  *   go in reverse. Also map the contiguous space a bit at a time to
@@ -167,79 +169,107 @@ static int coloring_cache_detect(void)
  */
 static void colored_copy(const struct jailhouse_memory_colored * col_mem)
 {
-	unsigned long phys_addr, tot_size, size;
-	int i;
-	
-	tot_size = col_mem->memory.size;
-
-	/* Find the first page that does not belong to the non-colored
-	 * mapping */
-	phys_addr = col_mem->memory.phys_start + tot_size;
-	
-	while (tot_size > 0) {
-		size = MIN(tot_size,
-			   NUM_TEMPORARY_PAGES * PAGE_SIZE);
-		
-		phys_addr -= size;
-		/* If we have reached the beginning (end of copy),
-		 * make sure we do not exceed the boundary of the
-		 * region */
-		if (phys_addr < col_mem->memory.phys_start)
-			phys_addr = col_mem->memory.phys_start;
-		
-		/* cannot fail, mapping area is preallocated */
-		paging_create(&this_cpu_data()->pg_structs, phys_addr,
-			      size, TEMPORARY_MAPPING_BASE,
-			      PAGE_DEFAULT_FLAGS,
-			      PAGING_NON_COHERENT | PAGING_NO_HUGE);
-
-		/* Actual data copy operation */
-		for (i = (size >> PAGE_SHIFT) - 1; i >= 0; --i) {
-			/* Destination: colored mapping created via HV_CREATE */
-			/* Source: non-colored mapping defined above */
-			memcpy((void *)ROOT_MAP_OFFSET + phys_addr + (i << PAGE_SHIFT),
-			       (void *)TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT),
-			       PAGE_SIZE);
+	if(SCHIM_RECOLORING_ENABLE) {
+//		col_print("\tCopy in progress\n");
+		unsigned long phys_addr, virt_addr, tot_size, size;
+		int i;
+//		col_print("\tline 176\n");
+		tot_size = col_mem->memory.size;
+//		col_print("\tline 178\n");
+		/* Find the first page that does not belong to the non-colored
+		 * mapping */
+		phys_addr = col_mem->memory.phys_start + tot_size;
+		virt_addr = col_mem->memory.virt_start + tot_size;
+//		col_print("\tline 182\n");
+		while (tot_size > 0) {
+			size = MIN(tot_size,
+				   NUM_TEMPORARY_PAGES * PAGE_SIZE);
+//		    col_print("\tline 186\n");
+			phys_addr -= size;
+			virt_addr -= size;
+			/* If we have reached the beginning (end of copy),
+			 * make sure we do not exceed the boundary of the
+			 * region */
+			if (phys_addr < col_mem->memory.phys_start)
+				phys_addr = col_mem->memory.phys_start;
+		   if (virt_addr < col_mem->memory.virt_start)
+			   virt_addr = col_mem->memory.virt_start;
+//			col_print("\tline 193\n");
+			/* cannot fail, mapping area is preallocated */
+			paging_create(&this_cpu_data()->pg_structs, phys_addr,
+				      size, TEMPORARY_MAPPING_BASE,
+				      PAGE_DEFAULT_FLAGS,
+				      PAGING_NON_COHERENT | PAGING_NO_HUGE);
+//		    col_print("\tline 199\n");
+			/* Actual data copy operation */
+			for (i = (size >> PAGE_SHIFT) - 1; i >= 0; --i) {
+				/* Destination: colored mapping created via HV_CREATE */
+				/* Source: non-colored mapping defined above */
+//				col_print("\t\t @ %d : memcpy(%lx, %lx, %x)\n", i, (ROOT_MAP_OFFSET + virt_addr + (i << PAGE_SHIFT)), (TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT)), PAGE_SIZE);
+//				col_print("\t\t dest = %lx + %lx + (%x << %x)\n", ROOT_MAP_OFFSET, phys_addr, i, PAGE_SHIFT);
+//				col_print("\t\t src  = %lx + (%x << %x)\n", TEMPORARY_MAPPING_BASE, i, PAGE_SHIFT);
+				//col_print("\t\t Content @ %lx = %x\n", phys_addr, *((unsigned*)phys_addr));
+//				memcpy((void *)ROOT_MAP_OFFSET + phys_addr + (i << PAGE_SHIFT),
+//				       (void *)TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT),
+//				       PAGE_SIZE);
+				memcpy((void *)ROOT_MAP_OFFSET + virt_addr + (i << PAGE_SHIFT),
+					   (void *)TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT),
+					   PAGE_SIZE);
+			}
+//			col_print("\tline 208\n");
+			tot_size -= size;
 		}
-		
-		tot_size -= size;
 	}
-	
+	else {
+		col_print("\tCopy skipped\n");
+	}
+
 }
 
 static void colored_uncopy(const struct jailhouse_memory_colored * col_mem)
 {
-	unsigned long phys_addr, tot_size, size;
-	int i;
-	
-	tot_size = col_mem->memory.size;
+	if(SCHIM_RECOLORING_ENABLE) {
+		col_print("\tUncopy in progress\n");
+		unsigned long phys_addr, virt_addr, tot_size, size;
+		int i;
 
-	/* Find the first page in the non-colored mapping */
-	phys_addr = col_mem->memory.phys_start;
-	
-	while (tot_size > 0) {
-		size = MIN(tot_size,
-			   NUM_TEMPORARY_PAGES * PAGE_SIZE);
-		
-		/* cannot fail, mapping area is preallocated */
-		paging_create(&this_cpu_data()->pg_structs, phys_addr,
-			      size, TEMPORARY_MAPPING_BASE,
-			      PAGE_DEFAULT_FLAGS,
-			      PAGING_NON_COHERENT | PAGING_NO_HUGE);
+		tot_size = col_mem->memory.size;
 
-		/* Actual data copy operation */
-		for (i = 0; i < (size >> PAGE_SHIFT); ++i) {
-			/* Destination: colored mapping created via HV_CREATE */
-			/* Source: non-colored mapping defined above */
-			memcpy((void *)TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT),
-			       (void *)ROOT_MAP_OFFSET + phys_addr + (i << PAGE_SHIFT),
-			       PAGE_SIZE);
+		/* Find the first page in the non-colored mapping */
+		phys_addr = col_mem->memory.phys_start;
+		virt_addr = col_mem->memory.virt_start;
+
+		while (tot_size > 0) {
+			size = MIN(tot_size,
+				   NUM_TEMPORARY_PAGES * PAGE_SIZE);
+
+			/* cannot fail, mapping area is preallocated */
+			paging_create(&this_cpu_data()->pg_structs, phys_addr,
+				      size, TEMPORARY_MAPPING_BASE,
+				      PAGE_DEFAULT_FLAGS,
+				      PAGING_NON_COHERENT | PAGING_NO_HUGE);
+
+			/* Actual data copy operation */
+			for (i = 0; i < (size >> PAGE_SHIFT); ++i) {
+				/* Destination: colored mapping created via HV_CREATE */
+				/* Source: non-colored mapping defined above */
+//				memcpy((void *)TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT),
+//				       (void *)ROOT_MAP_OFFSET + phys_addr + (i << PAGE_SHIFT),
+//				       PAGE_SIZE);
+			   memcpy((void *)TEMPORARY_MAPPING_BASE + (i << PAGE_SHIFT),
+				       (void *)ROOT_MAP_OFFSET + virt_addr + (i << PAGE_SHIFT),
+				       PAGE_SIZE);
+			}
+
+			phys_addr += size;
+			virt_addr += size;
+			tot_size -= size;
 		}
-
-		phys_addr += size;
-		tot_size -= size;
 	}
-	
+	else {
+		col_print("\tUncopy skipped\n");
+	}
+
 }
 
 
@@ -292,12 +322,12 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 	int ranges[MAX_COLORS*2];
 	ranges_in_mask(mask,MAX_COLORS,ranges);
 
-	
+
 	r = 0;
 	/* for (r = 0; r < (int)(col_mem.memory.size/f_offset); r++) */
 	while (virt_start < col_mem.memory.virt_start + col_mem.memory.size) {
 		for (k =0; k < MAX_COLORS*2; k+=2) {
-			
+
 			/* Calculate mem region */
 			if(ranges[k] == -1)
 				continue;
@@ -316,7 +346,7 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 				  frag_mem_region.virt_start, frag_mem_region.phys_start,
 				  frag_mem_region.size);
 #endif
-			
+
 			switch (type) {
 			case CREATE:
 				if (!(frag_mem_region.flags & (JAILHOUSE_MEM_COMM_REGION |
@@ -347,23 +377,23 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 						    frag_mem_region.size,
 						    frag_mem_region.virt_start + ROOT_MAP_OFFSET,
 						    PAGE_DEFAULT_FLAGS, PAGING_NON_COHERENT);
-				
+
 				if(err)
 					return err;
 				break;
-				
+
 			case SMMU_CREATE:
 
 				/* Throw an error if no SMMU mapping function was installed */
-				if (!col_ops.smmu_map_f) 
+				if (!col_ops.smmu_map_f)
 					return -ENOSYS;
-				
+
 				err = col_ops.smmu_map_f(cell, &frag_mem_region);
-				
+
 				if(err)
 					return err;
 				break;
-				
+
 			case DESTROY:
 				if (!JAILHOUSE_MEMORY_IS_SUBPAGE(&frag_mem_region)){
 					err = col_ops.unmap_f(cell, &frag_mem_region);
@@ -381,12 +411,12 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 				break;
 
 			case HV_DESTROY:
-				
+
 				err = paging_destroy(&this_cpu_data()->pg_structs,
 						     frag_mem_region.virt_start + ROOT_MAP_OFFSET,
 						     frag_mem_region.size,
 						     PAGING_NON_COHERENT);
-				
+
 				if(err)
 					return err;
 
@@ -398,18 +428,18 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 					return -ENOSYS;
 
 				/* TODO: implement this */
-				
+
 				break;
-				
+
 			case START:
 				if (frag_mem_region.flags & JAILHOUSE_MEM_LOADABLE) {
-					
+
 					/* Correct fragment geometry
 					 * to be located far away from
 					 * useful memory */
 					frag_mem_region.virt_start += ROOT_MAP_OFFSET;
-					
-					err = arch_unmap_memory_region(&root_cell, &frag_mem_region);					
+
+					err = arch_unmap_memory_region(&root_cell, &frag_mem_region);
 					if(err)
 						return err;
 				}
@@ -417,7 +447,7 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 
 			case LOAD:
 				if (frag_mem_region.flags & JAILHOUSE_MEM_LOADABLE) {
-					
+
 					/* Correct fragment geometry
 					 * to be located far away from
 					 * useful memory */
@@ -425,7 +455,7 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 
 					/* Create an ad-hoc mapping just to load this image */
 					err = arch_map_memory_region(&root_cell, &frag_mem_region);
-					
+
 					if(err)
 						return err;
 				}
@@ -436,7 +466,7 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 				region_size = frag_mem_region.size;
 
 				err = 0;
-				
+
 				while (region_size > 0) {
 					size = MIN(region_size,
 						   NUM_TEMPORARY_PAGES * PAGE_SIZE);
@@ -446,10 +476,10 @@ static int __manage_colored_region(const struct jailhouse_memory_colored col_mem
 						      size, TEMPORARY_MAPPING_BASE,
 						      PAGE_DEFAULT_FLAGS,
 						      PAGING_NON_COHERENT | PAGING_NO_HUGE);
-					
+
 					arm_dcaches_flush((void *)TEMPORARY_MAPPING_BASE, size,
 							  (enum dcache_flush)(extra));
-								       
+
 					region_addr += size;
 					region_size -= size;
 				}
@@ -478,7 +508,7 @@ int __coloring_cell_apply_to_col_mem(struct cell *cell, col_operation type, void
 		printk("ERROR: Colored regions exist but no suitable cache level found.\n");
 		return -ENODEV;
 	}
-	
+
 	for_each_col_mem_region(col_mem, cell->config, n) {
 		col_print("Colored OP %d: "
 			  "PHYS 0x%08llx -> VIRT 0x%08llx "
@@ -493,7 +523,7 @@ int __coloring_cell_apply_to_col_mem(struct cell *cell, col_operation type, void
 		if(err)
 			return err;
 	}
-	
+
 	return 0;
 }
 
@@ -514,7 +544,7 @@ static void coloring_cell_exit(struct cell *cell)
 			/* Create a linear mapping of the colored
 			 * region for the hypervisor */
 			err = __manage_colored_region(*col_mem, cell, HV_CREATE, NULL);
-			
+
 			if(err) {
 				col_print("ERROR: HV_CREATE returned %d\n", err);
  				return;
@@ -528,7 +558,7 @@ static void coloring_cell_exit(struct cell *cell)
 			col_print("\tPerfoming color rewinding of root-cell...\n");
 			colored_uncopy(col_mem);
 			col_print("\tDone!\n");
-			
+
 			/* Alroght. We can now release all the temporary mappings */
 			err = __manage_colored_region(*col_mem, cell, HV_DESTROY, NULL);
 
@@ -536,10 +566,10 @@ static void coloring_cell_exit(struct cell *cell)
 				col_print("ERROR: HV_DESTROY returned %d\n", err);
  				return;
 			}
-			
+
 		}
-	}	
-	
+	}
+
 }
 
 static void coloring_shutdown(void)
@@ -562,7 +592,7 @@ static int coloring_cell_init(struct cell *cell)
 			/* NOTE: we better have a working
 			 * coloring-aware SMMU here. */
 			err = __manage_colored_region(*col_mem, cell, HV_CREATE, NULL);
-			
+
 			if(err) {
 				col_print("ERROR: HV_CREATE returned %d\n", err);
  				return err;
@@ -576,7 +606,7 @@ static int coloring_cell_init(struct cell *cell)
 			col_print("\tPerfoming dynamic recoloring of root-cell...\n");
 			colored_copy(col_mem);
 			col_print("\tDone!\n");
-			
+
 			/* Alright. We can now release all the temporary mappings */
 			err = __manage_colored_region(*col_mem, cell, HV_DESTROY, NULL);
 
@@ -585,7 +615,7 @@ static int coloring_cell_init(struct cell *cell)
  				return err;
 			}
 		}
-	}	
+	}
 
 	/* Do this after the colored-copy, to reduce the likelihood
 	 * that it will fail due to a lack of pool pages needed to
@@ -594,21 +624,21 @@ static int coloring_cell_init(struct cell *cell)
 
 	if (err)
 		return err;
-	
+
 	return 0;
 }
-	
+
 static int coloring_init(void)
 {
 	int ret;
-	
+
 	/* Perform cache identification */
 	ret = coloring_cache_detect();
 
 	/* If unable to perform coloring, just skip this unit */
 	if (ret)
 		return 0;
-	
+
 	return coloring_cell_init(&root_cell);
 }
 
